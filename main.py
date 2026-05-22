c7c117d6d9cbf2375edabc2a3948b4f760d5cf2c
dogbogwog <266957916+dogbogwog@users.noreply.github.com>
2026-05-22 03:34:37 -0700

    style done

--- main.py
+++ main.py
@@ -140,4 +140,4 @@ def home(page_number):
     return render_template("index.html", items=sorted_data_list, current_page=page_number)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=5001, debug=True)
